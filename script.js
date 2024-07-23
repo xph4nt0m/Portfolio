0589d772eae375b8432bd027c0774d267c71e56f
xphant0m <xphant0m@MBP-de-xphant0m.lan>
2024-07-23 17:28:16 +0200

    fix animation nav bar et centrage h2

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var defaultTab = document.querySelector(".tab-content.default");
-    if (defaultTab) {
-        defaultTab.style.display = "flex";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  var defaultTab = document.querySelector(".tab-content.default");
+  if (defaultTab) {
+    defaultTab.style.display = "flex";
+  }
 
-    var navLinks = document.querySelectorAll(".nav li a");
-    navLinks.forEach(function(link) {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            var targetId = link.getAttribute("href").substring(1);
-            var targetContent = document.getElementById(targetId);
+  var navLinks = document.querySelectorAll(".nav li a");
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      var targetId = link.getAttribute("href").substring(1);
+      var targetContent = document.getElementById(targetId);
 
-            var allTabContents = document.querySelectorAll(".tab-content");
-            allTabContents.forEach(function(tab) {
-                tab.style.display = "none";
-            });
+      var allTabContents = document.querySelectorAll(".tab-content");
+      allTabContents.forEach(function (tab) {
+        tab.style.display = "none";
+      });
 
-            if (targetContent) {
-                targetContent.style.display = "flex";
-            }
-        });
+      if (targetContent) {
+        targetContent.style.display = "flex";
+      }
     });
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var navLinks = document.querySelectorAll('.nav-menu-item a');
+document.addEventListener("DOMContentLoaded", function () {
+  var navLinks = document.querySelectorAll(".nav-menu-item a");
 
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
 
-            navLinks.forEach(function(item) {
-                item.classList.remove('active');
-            });
+      navLinks.forEach(function (item) {
+        item.classList.remove("active");
+      });
 
-            link.classList.add('active');
-        });
+      link.classList.add("active");
     });
+  });
 });
-
-
